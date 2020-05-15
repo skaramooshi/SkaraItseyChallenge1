70f37afa766bc299abcaea6b6ea5dd9effb5f1b6
Jim <itsey@plisky.net>
2020-05-15 20:36:22 +0100

    Few extra unit tests and reformatting.

--- Source/SkaraItseyChallenge.Test/AuthorisationTests.cs
+++ Source/SkaraItseyChallenge.Test/AuthorisationTests.cs
@@ -8,15 +8,60 @@ namespace SkaraItseyChallenge.Test {
         protected Bilge b = new Bilge();
 
 
-        [Fact(DisplayName = nameof(TestMethod))]
+        [Fact(DisplayName = nameof(KnownBadLogin_Rejected))]
         [Trait(Traits.Age, Traits.Fresh)]
         [Trait(Traits.Style, Traits.Unit)]
-        public void TestMethod() {
+        public void KnownGoodLogin_Works() {
             b.Info.Flow();
 
+            string userName = "itsey";
+            bool loggedIn = false;
 
-            Assert.True(false, "Not coded");
+            // TODO : Implement Login Call here
+
+            Assert.True(loggedIn, "Good UserName Must Login");
         }
 
+
+        [Fact(DisplayName = nameof(KnownBadLogin_Rejected))]
+        [Trait(Traits.Age, Traits.Fresh)]
+        [Trait(Traits.Style, Traits.Unit)]
+        public void KnownBadLogin_Rejected() {
+            b.Info.Flow();
+            string userName = "mrHaxxor";
+            bool loggedIn = false;
+
+            // TODO : Implement Login Call Here
+
+            Assert.False(loggedIn, "Bad UserName Must Not Login");
+        }
+
+
+
+        [Fact(DisplayName = nameof(NullLogin_ThrowsException))]
+        [Trait(Traits.Age, Traits.Fresh)]
+        [Trait(Traits.Style, Traits.Unit)]
+        public void NullLogin_ThrowsException() {
+            b.Info.Flow();
+            
+            string userName = string.Empty;            
+
+            Assert.Throws<ArgumentNullException>(() => {
+                // TODO : Implement Login Call here
+            });
+        }
+
+
+        [Fact(DisplayName = nameof(EmptyLogin_ThrowsException))]
+        [Trait(Traits.Age, Traits.Fresh)]
+        [Trait(Traits.Style, Traits.Unit)]
+        public void EmptyLogin_ThrowsException() {
+            b.Info.Flow();
+            string userName = string.Empty;
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => {
+                // TODO : Implement Login Call here
+            });
+        }
     }
 }
--- Source/SkaraItseyChallenge/Program.cs
+++ Source/SkaraItseyChallenge/Program.cs
@@ -1,20 +1,15 @@
 ﻿using System;
 
-namespace SkaraItseyChallenge1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace SkaraItseyChallenge1 {
+    class Program {
+        static void Main(string[] args) {
             var name = "";
 
-            while (name != "itsey")
-            {
+            while (name != "itsey") {
                 Console.WriteLine("What is your name?");
                 name = Console.ReadLine();
 
-                if (name != "itsey")
-                {
+                if (name != "itsey") {
                     Console.WriteLine("You are not authorised {0}", name);
                 }
             }
